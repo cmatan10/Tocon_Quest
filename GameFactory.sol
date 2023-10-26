e923cbd5734be3ef3703341b94dbeec61a096bc7
cmatan10 <cmatan10@gmail.com>
2023-10-26 10:19:54 +0300

    secound commit

--- GameFactory.sol
+++ GameFactory.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "./Helpers/FactoryHelper.sol";
 
-contract GameFactory is Initializable, OwnableUpgradeable, FactoryHelper {
+contract GameFactory is Initializable, OwnableUpgradeable {
 
     bool public paused;
     uint public games;
@@ -13,20 +13,29 @@ contract GameFactory is Initializable, OwnableUpgradeable, FactoryHelper {
     event DeployInstance(address indexed Instance, address indexed sender, uint indexed game );
 
     function initialize() external initializer{
-        __Ownable_init(msg.sender);
+        __Ownable_init();
         paused = false;
     }
 
-    function deploy(uint game) external returns (address addr) { 
+    function deploy(uint game) external returns (address addr, address hackAddr) { 
         require(!paused, "The contract is paused!");
         require(game > 0 && game <= games, "There Is No More Games");
-        bytes memory bytecode = ChooseGame(game);
+        bytes memory bytecode = FactoryLib.ChooseGame(game);
         assembly {
 
             addr := create(callvalue(), add(bytecode, 0x20), mload(bytecode))
         }
         require(addr != address(0), "deploy failed");
         emit DeployInstance(addr , msg.sender, game);
+        
+        bytes memory hackBytecode = FactoryLib.ChooseHackingGame(game);
+        if (hackBytecode.length > 0) {
+            assembly {
+                hackAddr := create(callvalue(), add(hackBytecode, 0x20), mload(hackBytecode))
+            }
+            require(hackAddr != address(0), "Hack deploy failed");
+            emit DeployInstance(hackAddr, msg.sender, game);
+        }
     }
 
     function setGames(uint num) external onlyOwner{
@@ -36,5 +45,4 @@ contract GameFactory is Initializable, OwnableUpgradeable, FactoryHelper {
     function setPaused(bool _state) external onlyOwner {
         paused = _state;
     }
-    
-}
+}        

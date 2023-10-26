93d53f74551c90f0adf9f006f7b350707d21b29d
cmatan10 <86375957+cmatan10@users.noreply.github.com>
2023-10-26 14:08:20 +0300

    Update InterfaceId.sol

--- Games/InterfaceId.sol
+++ Games/InterfaceId.sol
@@ -8,9 +8,4 @@ contract InterfaceId {
         require(id == bytes4(keccak256("CalcMe(bytes4)")), "Calc Me Again!");
         answer = true;
     }
-
-    
-function Calc() public pure returns(bytes4){
-   return bytes4(keccak256("CalcMe(bytes4)"));
-}
 }
